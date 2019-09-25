a1525faa19e97ee74fff004a2580c545aaea2ace
Bruno Andrade <brunnoandradi@gmail.com>
2019-09-25 17:48:39 -0300

    Persistindo Customer e inserindo TryCatch

--- src/backoffice/models/customer.model.ts
+++ src/backoffice/models/customer.model.ts
@@ -1,12 +1,14 @@
 import { Address } from './address.model';
 import { CreditCard } from './credit-card.model';
 import { User } from './user.model';
+import { Pet } from './pet.model';
 
 export class Customer {
     constructor(
         public name: string,
         public document: string,
         public email: string,
+        public pets: Pet[],
         public billingAddress: Address,
         public shippingAddress: Address,
         public creditCard: CreditCard,
